7528b8a3970691f3a0948db84209b346a70a3fd5
HaoLieu <lieuquochao@gmail.com>
2023-06-08 14:51:21 +0700

    implement list-product

---
+++ frontend/src/data.js
@@ -0,0 +1,50 @@
+const data = {
+    product: [
+        {
+            name: 'Adidas shoe',
+            slug: 'adidas-shoe',
+            category: 'Shoes',
+            image: '/images/adidas-shoe1.jpg',
+            price: 120,
+            countInStock: 10,
+            brand: 4.5,
+            numReviews: 10,
+            description: 'an adidas shoe'
+        },
+        {
+            name: 'Puma shoe',
+            slug: 'puma-shoe',
+            category: 'Shoes',
+            image: '/images/puma-shoe1.jpg',
+            price: 100,
+            countInStock: 10,
+            brand: 4.0,
+            numReviews: 9,
+            description: 'a puma shoe'
+        },
+        {
+            name: 'Nike short 1',
+            slug: 'nike-short-1',
+            category: 'Shorts',
+            image: '/images/nike-short-black.jpg',
+            price: 90,
+            countInStock: 10,
+            brand: 4.2,
+            numReviews: 10,
+            description: 'a Nike short'
+        },
+        {
+            name: 'Nike short 2',
+            slug: 'nike-short-2',
+            category: 'Shorts',
+            image: '/images/nike-short-black.jpg',
+            price: 90,
+            countInStock: 10,
+            brand: 4.2,
+            numReviews: 10,
+            description: 'a Nike short'
+        }
+    ]
+}
+
+export default data
